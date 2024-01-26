4067dec1be344e375074ad5e2e46d5277957d502
unknown <OllyLicoly>
2024-01-26 15:12:51 +0700

    Add project

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,46 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+import java.util.List;
+
 
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+    public static void main(String[] args) throws SQLException {
+//        Util.getConnection();
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Leonard", "Hofstadter", Byte.parseByte("25"));
+        userService.saveUser("Sheldon", "Cooper", Byte.parseByte("23"));
+        userService.saveUser("Howard", "Wolowitz", Byte.parseByte("24"));
+        userService.saveUser("Rajesh", "Koothrappali", Byte.parseByte("26"));
+
+        List<User> userList = userService.getAllUsers();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        userService.removeUserById(3);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+        List<User> userList1 = userService.getAllUsers();
+        for (User user : userList1) {
+            System.out.println(user);
+        }
+
+
+
+
     }
+
+
 }
